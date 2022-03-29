3be6f13597ca58e9f623800ce17071fd3ab736af
noahaklem <83715460+noahaklem@users.noreply.github.com>
2022-03-29 16:34:40 -0600

    solutions

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
-  // type your code here
+  let newString = "";
+  for(let i = str.length -1; i >= 0; i--){
+    newString += str[i];
+  };
+  return newString
 }
 
 if (require.main === module) {
@@ -16,4 +20,17 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+  // 1. write a function nameed reverseString
+  //   function should take in an string argument
+  //   function should loop through the string
+  // 2. function should return a reversed string
 // And a written explanation of your solution
+
+// set a new string equal to an empty string
+// build the loop
+  // set i equal to the string length
+  // str[0] is the first char, so i needs to be greater than or equal to 0
+  // then decrement the i counter to count down to 0
+    // enter the loop where str[i] should be str[5] then str[4], etc
+    // all I need to do now is concat to the newString var
+// after the loop is done return the string
--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,5 +1,9 @@
 def reverse_string(str)
-  # type your code in here
+  reversed = ""
+  str.chars.each do |c|
+    reversed = c + reversed
+  end
+  reversed
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,8 +15,23 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ybabtac'"
   puts "=>", reverse_string('catbaby')
 
-  # Don't forget to add your own!
+  puts 
+  
+  puts "Expecting: 'elloh'"
+  puts "=>", reverse_string('hello')
 end
 
 # Please add your pseudocode to this file
+# build a functin called reverse_string
+#   function takes an argument of a string 
+#   for each letter in string 
+#     add to a new string
+#   end
+#   return the new string
+# end
 # And a written explanation of your solution
+
+# set a new_string variable equal to an empty string
+#   you need to seperate each character in the string
+#   for each character in the string want to add it to the beginning of the new_string
+# then return the new_string variable once finished
